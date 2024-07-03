26979b3d3ffaa69eef5a17c4763ddf8be57aea5a
Mohamed-Abbas-Homani <mohamedabbashoumani@gmail.com>
2024-07-03 12:15:17 +0300

    initial

---
+++ database/database.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"tcpserver/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+	err = DB.AutoMigrate(&models.User{})
+	if err != nil {
+		return
+	}
+
+	log.Println("Database connected")
+}
